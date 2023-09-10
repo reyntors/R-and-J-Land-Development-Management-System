778be6e1a8ee802f6889b9f2b217a36525698354
Reynard Z. Torculas <97606356+reyntors@users.noreply.github.com>
2023-09-10 14:08:56 +0800

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 
 const PORT = 3000;
@@ -9,6 +10,11 @@ const PORT = 3000;
 app.get('/', (req, res)=>{
     res.send('Hello Node API')
 })
+
+app.get('/blog', (req, res)=>{
+    res.send('Hello Blog my name is Reynard')
+})
+
 
 app.listen(PORT, () =>{
     console.log(`Server is listen on PORT ${PORT}`)
