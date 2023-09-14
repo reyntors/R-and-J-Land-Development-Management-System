ae8e5d054f0d593242b3a10be10c4bf13331c09b
KarlBorromeo <98805305+KarlBorromeo@users.noreply.github.com>
2023-09-14 22:15:19 +0800

    setup routes done

--- src/route.js
+++ src/route.js
@@ -1,6 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 import HomePage from './components/Pages/HomePage/HomePage.vue'
+import AboutPage from './components/Pages/AboutPage/AboutPage.vue'
+import ProjectPage from './components/Pages/ProjectPage/ProjectPage.vue'
+import GalleryPage from './components/Pages/GalleryPage/GalleryPage.vue'
+import FormPage from './components/Pages/FormPage/FormPage.vue'
+import ContactPage from './components/Pages/ContactPage/ContactPage.vue'
 
 const route = createRouter({
 
@@ -8,10 +13,11 @@ const route = createRouter({
         routes: [
             {path: '/', redirect: '/home'},
             {path:'/home', component: HomePage},
-            {path:'/about', component: null},
-            {path:'/gallery', component: null},
-            {path:'/forms', component: null},
-            {path:'/contact', component: null},
+            {path:'/about', component: AboutPage},
+            {path:'/projects', component: ProjectPage},
+            {path:'/gallery', component: GalleryPage},
+            {path:'/forms', component: FormPage},
+            {path:'/contact', component: ContactPage},
             {path:'/:notFound(.*)', component: null}
         ]
     })
