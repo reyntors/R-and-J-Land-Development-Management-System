2e58242bfcf157f883ecf3903fd778747a7c4352
Reynard Z. Torculas <reyntors2@gmail.com>
2024-01-31 09:46:47 -0800

    added token when register

--- server/services/users.services.js
+++ server/services/users.services.js
@@ -33,20 +33,28 @@ async function login({username, password, roles}, callback){
             callback(null, {...user.toJSON(), token});
         } 
 
-async function register(params, callback) {
-    if(params.username === undefined) {
-        return callback({ message: "Username Required"});
-    }
-
-    const user = new User(params);
-    user.save()
-    .then((response) => {
-        return callback(null, response);
-    })
-    .catch((error) => {
-        return callback(error);
-    });
-}
+    async function register(params, callback) {
+            if(params.username === undefined) {
+                return callback({ message: "Username Required"});
+            }
+        
+            const user = new User(params);
+            user.save()
+            .then((response) => {
+                // Generate token for the newly registered user
+                const token = jwt.sign({ 
+                    username: user.username, 
+                    roles: user.roles 
+                }, 
+                process.env.JWT_SECRET_KEY, { 
+                    expiresIn: '24h' 
+                });
+                return callback(null, {...response.toJSON(), token});
+            })
+            .catch((error) => {
+                return callback(error);
+            });
+        }
 
 module.exports = {
     login,
